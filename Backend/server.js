78fded4b41683b7b377725c99137fc296d139cbe
HarishKarthickS <harishskarthick.s@gmail.com>
2024-03-23 12:26:50 +0530

    Initializing mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,29 @@
-const express=require("express")
-const app=express()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const { startDatabase, isConnected } = require('./db/database');
+const app = express()
+app.use(bodyParser.json());
+app.use(cors())
+app.use(express.json())
 
-app.get("/home",(req,res)=>{
-    res.send("hallo every one")
+
+app.get('/', (req, res) => {
+  res.send({message:'Welcome to the amazing world of Tech'})
+})
+app.get('/home', (req, res) => {
+  res.json({
+    message: isConnected() ? 'Database is connected' : 'Disconnected from database'
+  })
+  })
+app.get('/ping',(req,res)=>{
+    res.send({message:'pong!, Welcome to the amazing world of Tech'})
 })
 
-app.listen(3000,()=>{
+startDatabase()
+.then(()=>{
+  app.listen(3000,async()=>{
     console.log('Starting Server ....🚀')
     console.log('Server started running on port 3000 🏃‍♂️')
+})
 })
