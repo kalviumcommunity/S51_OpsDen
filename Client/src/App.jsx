68f7cdc596ebe5e889133101374a380f85effe49
JEEVEEKA <jeeveeka.ps@kalvium.community>
2024-03-23 14:39:07 +0530

    made changes in navbar

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,13 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1>Welcome to OpsDen</h1>
+      <Navbar/>
     </>
   )
 }
