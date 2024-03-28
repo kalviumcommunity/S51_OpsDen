acf2ad6b98f848ae1abb8106bff41889ae7b7bfd
HarishKarthickS <harishskarthick.s@gmail.com>
2024-03-28 12:02:29 +0530

    Bug Fix - 1

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Main from './Components/Main'
 import Home from './Components/Home'
 import SignInSignUpForm from './Components/Signup'
+import './App.css'
 
 
 function App() {
