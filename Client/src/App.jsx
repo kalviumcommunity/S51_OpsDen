4cbf01194111f85c8e45ef50d287702fdf09436e
HarishKarthickS <harishskarthick.s@gmail.com>
2024-04-01 11:11:06 +0530

    Update routing src

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,8 +10,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/frontPage' element={<Main/>}></Route>
+          <Route path='/' element={<Main/>}></Route>
           <Route path='/signup' element={<SignInSignUpForm/>}></Route>
         </Routes>
       </BrowserRouter>
--- Client/src/Components/Home.jsx
+++ Client/src/Components/Home.jsx
@@ -5,21 +5,7 @@ import { Link } from 'react-router-dom';
 import '../App.css'
 
 function Home() {
-  return (
-    <div id='body'>
-      <div className="video-background">
-        <video autoPlay loop muted>
-          <source src={Video} type="video/mp4" />
-        </video>
-      </div>
-      <div id='content'>
-        <img className="logo" src={Logo} alt="logo" />
-        <p className='welcome'>Welcome to OpsDen</p>
-        <Link to="/signup"><button className='login'>Login</button></Link>
-      </div>
-      <footer className='copy'>&copy; Copyright of OpsDen from 2024 - 2082</footer>
-    </div>
-  );
+
 }
 
 export default Home;
