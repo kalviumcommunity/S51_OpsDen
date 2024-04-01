a2f1650810bf84f00f30d4cc8b12cc1bb3905e63
HarishKarthickS <harishskarthick.s@gmail.com>
2024-04-01 11:13:27 +0530

    Update routing src

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,5 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Main from './Components/Main'
-import Home from './Components/Home'
 import SignInSignUpForm from './Components/Signup'
 import './App.css'
 
